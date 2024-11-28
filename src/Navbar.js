9cd428d2f5880f49879494029e9a5a7cc8b7b3a7
unknown <md.monirulmunna.cse@gmail.com>
2024-11-28 17:08:54 +0600

    first commit

---
+++ src/Navbar.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="navbar-brand">
+        <Link to="/">MyApp</Link>
+      </div>
+      <ul className="navbar-links">
+        <li>
+          <Link to="/signup">Signup</Link>
+        </li>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+        <li>
+          <Link to="/dashboard">Dashboard</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
